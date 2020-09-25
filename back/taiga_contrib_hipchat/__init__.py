f58c780314aba6466a763969f5507adca2aa5050
Miguel Gonzalez <migonzalvar@gmail.com>
2020-09-25 16:59:25 +0200

    Set actual current version

--- back/taiga_contrib_hipchat/__init__.py
+++ back/taiga_contrib_hipchat/__init__.py
@@ -16,6 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = (3, 1, 0)
+__version__ = (5, 5, 0)
 
 default_app_config = "taiga_contrib_hipchat.apps.TaigaContribHipChatAppConfig"
